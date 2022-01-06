3c89cac2e5e56a44c6a0bd3a0942b1c55ebbe061
Lars Lockefeer <lars.lockefeer@gmail.com>
2022-01-06 22:26:36 +0100

    Remove log statements

--- util/DailyNoteParser.ts
+++ util/DailyNoteParser.ts
@@ -5,10 +5,7 @@ import path from 'path';
 export const extractDueDateFromDailyNotesFile = (filePath: string): DateTime | undefined => {
   const dailyNotesSettings = getDailyNoteSettings();
   if (dailyNotesSettings.folder && path.dirname(filePath) == dailyNotesSettings.folder) {
-    console.log(`Found a daily note ${filePath}`);
     const dueDate = getDateFromPath(filePath, 'day');
-    console.log(dueDate);
-    console.log(DateTime.fromISO(dueDate.toISOString()));
     return DateTime.fromISO(dueDate.toISOString());
   }
   return undefined;
